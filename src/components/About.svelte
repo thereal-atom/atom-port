746236848bcf593b5a891128d65b24f0d044d0b0
thereal-atom <oscarfal2006@gmail.com>
2022-09-03 18:32:42 +0100

    updated about section

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,19 +1,11 @@
-<!-- <div class="section">
-    <div class="w-10/12">
-        <h1 class="text-7xl heading">About Me</h1>
-        <p class="description">I started coding at the beginning of 2021 during lockdown. At this time I had started trading crypto currency and some of my friends wanted a discord bot so i learned NodeJS and Discord.js. After a while I started coding websites and servers turning me into a full stack developer.</p>    
-        <div class="line w-full opacity-10 mt-16" id="about"><p class="opacity-0">a</p></div>
-    </div>
-</div>
-<style>
-    .line{
-        border-top: solid 1px;
-    } 
-</style> -->
-
 <div class="container">
     <div class="section-title" id="about">About Me</div>
-    <div class="section-text">I started coding at the beginning of 2021 during lockdown. At this time I had started trading crypto currency and some of my friends wanted a discord bot so i learned NodeJS and Discord.js. After a while I started coding websites and servers, becoming a full stack developer. In 2022 I became interested in DevOps and setup projects with CI/CD, observability, containerization and orchestration.</div>
+    <div class="section-text">
+        While I'm young, with not much experience, I learn quick and I am always willing to take on challenging tasks and learn more.
+        Like any human I make mistakes, but I have taught myself to never repeat the same mistake twice - no matter the outcome of a situation I learn something valuable and as such I am not scared of failure.
+        I make logical and data driven decisions and even in situations where this not possible, I make decisions based on my intuition and experience.
+        Along with this, I work well in high pressure situations and handle stress well.
+    </div>
 </div>
 
 <style>
